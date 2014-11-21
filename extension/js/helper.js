ff34e080c1df6c5260eef1a8de61cb44f4fdbc7f
Haohan Yang <ysyanghaohan@gmail.com>
2014-11-20 19:46:06 -0800

    Decompose main.js into multiple files for code clarity

---
+++ extension/js/helper.js
@@ -0,0 +1,103 @@
+// load location data with sync ajax request
+function loadLocations(locations) {
+	$.ajax({
+		url: "http://www.studentuniverse.com/grails/geolocation/getLocations",
+		async: false,
+		dataType: "text", // prevent jQuery from converting the data
+		success: function(data, textStatus, jqXHR) {
+			if (jqXHR.status !== 200) {
+				errorHandler(jqXHR.status + " " + jqXHR.statusText);
+				return;
+			}
+			var items = JSON.parse(data);
+			items.forEach(function(item) {
+				if (item.displayName && item.code) {
+					locations.push(item);
+				}
+			});
+		},
+		error: function(jqXHR) {
+			errorHandler(jqXHR.status + " " + jqXHR.statusText);
+		},
+	});
+
+	function errorHandler(msg) {
+		var $error = $("<div/>").attr("class", "alert alert-danger").attr("role", "alert");
+		$error.html("Failed to fetch airports info from server: "
+			+ "<strong>" + msg + "</strong>"
+			+". Please try it later.");
+		$("#error").html($error);
+		$("form, table").hide();
+	}
+}
+
+// generate location dropdown menu for binded location input box
+var generateLocationList = function() {
+	var prevStr = "";
+
+	function escapeRegExp(string) {
+		return string.replace(/([.*+?^${}()|\[\]\/\\])/g, "\\$1");
+	}
+
+	// for dropdown menu items
+	function mouseEnterHandler(e) {
+		$(this).siblings(".active").removeClass("active");
+		$(this).addClass("active");
+	}
+
+	// for dropdown menu items
+	function clickHandler(e) {
+		var $input = $(this).parents(".dropdown").siblings(".location");
+		$input.val(this.textContent);
+		$input.focus();
+		var $menu = $(this).parent();
+		$menu.hide().empty();
+	}
+
+	return function(locations) {
+		var str = this.value;
+		// if the string didn't change, do nothing
+		if (str === prevStr) {
+			return;
+		}
+		prevStr = str;
+		// clear current list if it exists
+		var $menu = $(this).parent().find(".dropdown-menu");
+		$menu.hide().empty();
+
+		str = str.trim();
+		// don't give prompts when there are less than 3 characters
+		if (str.length < 3) {
+			return;
+		}
+
+		locations.forEach(function(loc) {
+			if (new RegExp(escapeRegExp(str), "i").test(loc.displayName)) {
+				var item = loc.displayName.replace(new RegExp("(" + escapeRegExp(str) + ")", "gi"), "<strong>$1</strong>");
+				var $li = $("<li/>").html(item);
+				str.toUpperCase() === loc.code ? $menu.prepend($li) : $menu.append($li);
+			}
+			else {
+				var tokens = str.split(/\s+/);
+				var contained = true;
+				for (var i = 0; i < tokens.length; i++) {
+					if (!new RegExp(escapeRegExp(tokens[i]), "i").test(loc.displayName)) {
+						contained = false;
+						break;
+					}
+				}
+				if (contained) {
+					var $li = $("<li/>").html(loc.displayName);
+					$menu.append($li);
+				}
+			}
+		});
+
+		if ($menu.html()) {
+			$menu.children().first().addClass("active");
+			$menu.children().mouseenter(mouseEnterHandler);
+			$menu.children().click(clickHandler);
+			$menu.show();
+		}
+	};
+}();
--- extension/js/main.js
+++ extension/js/main.js
@@ -5,51 +5,7 @@ bg = chrome.extension.getBackgroundPage();
 	// array to store info of cities and airports
 	var locations = [];
 
-	// encapsulate operations on result table
-	var resultTable = {
-		$table: $("table"),
-
-		reset: function() {
-			this.$table.html("<thead></thead><tbody></tbody>");
-			this.rowNum = 0;
-
-			var $tr = $("<tr/>");
-			$tr.append("<th class='col-xs-1 col-number'>#</th>");
-			$tr.append("<th class='col-xs-2 col-depart'>Depart</th>");
-			if (!$("#oneWay").prop("checked")) {
-				$tr.append("<th class='col-xs-2 col-return'>Return</th>");
-			}
-			$tr.append("<th class='col-offer'>Offer</th>");
-			$tr.append("<th class='col-duration'>Duration</th>");
-			$tr.append("<th class='col-status'>Status</th>");
-
-			this.$table.children("thead").append($tr);
-		},
-
-		addRow: function(values) {
-			this.rowNum++;
-
-			var $headings = this.$table.find("th");
-			values.unshift(this.rowNum); // insert row number at the start
-			while (values.length < $headings.length) {
-				values.push("");
-			}
-
-			var $tr = $("<tr/>");
-			$tr.attr("id", "query" + this.rowNum);
-			for (var i = 0; i < $headings.length; i++) {
-				var $td = $("<td/>");
-				$td.addClass($headings.eq(i).attr("class"));
-				$td.text(values[i]);
-				$tr.append($td);
-			}
-
-			this.$table.children("tbody").append($tr);
-			return $tr;
-		}
-	};
-
-	getLocations();
+	loadLocations(locations);
 
 	$(".location").keydown(keyDownHandler);
 	$(".location").keyup(keyUpHandler);
@@ -61,43 +17,6 @@ bg = chrome.extension.getBackgroundPage();
 
 	///////////////////////////////////////
 	// function implementation
-
-	function getLocations() {
-		$.ajax({
-			url: "http://www.studentuniverse.com/grails/geolocation/getLocations",
-			async: false,
-			dataType: "text", // prevent jQuery from converting the data
-			success: function(data, textStatus, jqXHR) {
-				if (jqXHR.status !== 200) {
-					errorHandler(jqXHR.status + " " + jqXHR.statusText);
-					return;
-				}
-				var items = JSON.parse(data);
-				items.forEach(function(item) {
-					if (item.displayName && item.code) {
-						locations.push(item);
-					}
-				});
-			},
-			error: function(jqXHR) {
-				errorHandler(jqXHR.status + " " + jqXHR.statusText);
-			},
-		});
-
-		function errorHandler(msg) {
-			var $error = $("<div/>").attr("class", "alert alert-danger").attr("role", "alert");
-			$error.html("Failed to fetch airports info from server: "
-				+ "<strong>" + msg + "</strong>"
-				+". Please try it later.");
-			$("#error").html($error);
-			$("form, table").hide();
-		}
-	}
-
-	// function blurHandler(e) {
-	// 	var $menu = $(this).parent().find(".dropdown-menu");
-	// 	$menu.hide().empty();
-	// }
 
 	function keyDownHandler(e) {
 		var $menu = $(this).parent().find(".dropdown-menu");
@@ -143,7 +62,7 @@ bg = chrome.extension.getBackgroundPage();
 			}
 		}
 		else {
-			generateLocationList.bind(this, e)();
+			generateLocationList.bind(this, locations)();
 		}
 
 		function focusNextInputField(currInput) {
@@ -167,76 +86,6 @@ bg = chrome.extension.getBackgroundPage();
 			$(this).hide().empty();
 		});
 	}
-
-	var generateLocationList = function() {
-		var prevStr = "";
-
-		function escapeRegExp(string) {
-			return string.replace(/([.*+?^${}()|\[\]\/\\])/g, "\\$1");
-		}
-
-		// for dropdown menu items
-		function mouseEnterHandler(e) {
-			$(this).siblings(".active").removeClass("active");
-			$(this).addClass("active");
-		}
-
-		// for dropdown menu items
-		function clickHandler(e) {
-			var $input = $(this).parents(".dropdown").siblings(".location");
-			$input.val(this.textContent);
-			$input.focus();
-			var $menu = $(this).parent();
-			$menu.hide().empty();
-		}
-
-		return function(e) {
-			var str = this.value;
-			// if the string didn't change, do nothing
-			if (str === prevStr) {
-				return;
-			}
-			prevStr = str;
-			// clear current list if it exists
-			var $menu = $(this).parent().find(".dropdown-menu");
-			$menu.hide().empty();
-
-			str = str.trim();
-			// don't give prompts when there are less than 3 characters
-			if (str.length < 3) {
-				return;
-			}
-
-			locations.forEach(function(loc) {
-				if (new RegExp(escapeRegExp(str), "i").test(loc.displayName)) {
-					var item = loc.displayName.replace(new RegExp("(" + escapeRegExp(str) + ")", "gi"), "<strong>$1</strong>");
-					var $li = $("<li/>").html(item);
-					str.toUpperCase() === loc.code ? $menu.prepend($li) : $menu.append($li);
-				}
-				else {
-					var tokens = str.split(/\s+/);
-					var contained = true;
-					for (var i = 0; i < tokens.length; i++) {
-						if (!new RegExp(escapeRegExp(tokens[i]), "i").test(loc.displayName)) {
-							contained = false;
-							break;
-						}
-					}
-					if (contained) {
-						var $li = $("<li/>").html(loc.displayName);
-						$menu.append($li);
-					}
-				}
-			});
-
-			if ($menu.html()) {
-				$menu.children().first().addClass("active");
-				$menu.children().mouseenter(mouseEnterHandler);
-				$menu.children().click(clickHandler);
-				$menu.show();
-			}
-		};
-	}();
 
 	function initializeDatePicker() {
 		$(".date").datepicker({
@@ -312,89 +161,5 @@ bg = chrome.extension.getBackgroundPage();
 		return true;
 	}
 
-	function buildQueries() {
-		var commonData = {
-			// tripType: "2",
-			// origin: "LAX",
-			// origin2: "",
-			// destination: "BJS",
-			// destination2: "",
-			// numTravelers: "1",
-			// departs: "10/23/2014",
-			// returns: "10/29/2014",
-			// // return_time: "anytime",
-			// // departure_time: "anytime",
-			// // product: "flight",
-			productType: "flight",
-		};
-
-		commonData.tripType = Number($(".tripType:checked").val());
-		commonData.origin = $("#origin").attr("data-code");
-		commonData.destination = $("#destination").attr("data-code");
-		commonData.departs = $("#departs").val();
-		commonData.numTravelers = $("#numTravelers").val();
-
-		var queries = [];
-		var url = "http://www.studentuniverse.com/app/product/search";
-		var maxPrice = Number($("#maxPrice").val());
-
-		if (commonData.tripType === 1) {
-			var departStart = -Number($("#departDaysBefore").val());
-			var departEnd = Number($("#departDaysAfter").val());
-
-			for (var i = departStart; i <= departEnd; i++) {
-				var data = JSON.parse(JSON.stringify(commonData));
-				data.departs = getDateAfterDays(data.departs, i);
-
-				var $tr = resultTable.addRow([data.departs]);
-				var query = new Query($tr, url, data, maxPrice);
-				query.start();
-				// setTimeout(query.start.bind(query), (departEnd - departStart) / 10 * 1000 * (count - 1));
-				queries.push(query);
-			}
-		}
-		else  {
-			commonData.returns = $("#returns").val();
-			if (commonData.tripType === 3) {
-				// TODO:
-				// commonData.origin2 = 
-				// commonData.destination2 = 
-			}
-
-			var departStart = -Number($("#departDaysBefore").val());
-			var departEnd = Number($("#departDaysAfter").val());
-			var returnStart = -Number($("#returnDaysBefore").val());
-			var returnEnd = Number($("#returnDaysAfter").val());
-
-			for (var i = departStart; i <= departEnd; i++) {
-				for (var j = returnStart; j <= returnEnd; j++) {
-					var data = JSON.parse(JSON.stringify(commonData));
-					data.departs = getDateAfterDays(data.departs, i);
-					data.returns = getDateAfterDays(data.returns, j);
-
-					var $tr = resultTable.addRow([data.departs, data.returns]);
-					var query = new Query($tr, url, data, maxPrice);
-					query.start();
-					// setTimeout(query.start.bind(query), (departEnd - departStart) * (returnEnd - returnStart) / 20 * 1000 * (count - 1));
-					queries.push(query);
-				}
-			}
-		}
-
-		return queries;
-	}
-
-	function getDateAfterDays(date, number) {
-		date = new Date(date);
-		date.setDate(date.getDate() + number);
-
-		var mm = date.getMonth() + 1;
-		mm = mm < 10 ? "0" + mm : mm;
-		var dd = date.getDate();
-		dd = dd < 10 ? "0" + dd : dd;
-		var yyyy = date.getFullYear();
-
-		return [mm, dd, yyyy].join("/");
-	}
-
+	function checkReturnDate() {}
 })();
